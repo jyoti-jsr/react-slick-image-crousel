bedf92c1b90c3ea24ef6693d7e4dbaefeae6ee66
Jyoti Ranjan Giri <jyotiranjangiri.jsr@gmail.com>
2023-06-19 19:18:20 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Slider from "react-slick";
+import "./index.css";
+
+class ReactSlickDemo extends React.Component {
+  render() {
+    var settings = {
+      dots: true,
+      arrows: false,
+      autoplay: true
+    };
+    return (
+      <div className="container">
+        <Slider {...settings}>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+          <div>
+            <img src="http://placekitten.com/g/400/200" />
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ReactSlickDemo />, document.getElementById("container"));
